d4ac462189c4b6df7486d9d04dda95160dd8bb4e
Paul Landers <paul.landers@testplant.com>
2015-04-07 11:32:03 -0600

    added dot files, and xcode user state

---
+++ dot/setup_dots.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+ln -si ~/env/dot/bash_profile ~/.bash_profile
+ln -si ~/env/dot/bashrc ~/.bashrc
+ln -si ~/env/dot/gitconfig ~/.gitconfig
+ln -si ~/env/dot/gitignore_global ~/.gitignore_global
+ln -si ~/env/dot/UserData ~/Library/Developer/Xcode/UserData
+
+
+chflags nohidden ~/Library
+
+
