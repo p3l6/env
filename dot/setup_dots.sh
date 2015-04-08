e2841f3950fe2ece2c68d46b306041d7ad87af9b
Paul Landers <paul.landers@testplant.com>
2015-04-08 14:51:21 -0600

    fixed setup_dots to make better links

--- dot/setup_dots.sh
+++ dot/setup_dots.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-ln -si ~/env/dot/bash_profile ~/.bash_profile
-ln -si ~/env/dot/bashrc ~/.bashrc
-ln -si ~/env/dot/gitconfig ~/.gitconfig
-ln -si ~/env/dot/gitignore_global ~/.gitignore_global
-ln -si ~/env/dot/UserData ~/Library/Developer/Xcode/UserData
-
+ln -sih ~/env/dot/bash_profile ~/.bash_profile
+ln -sih ~/env/dot/bashrc ~/.bashrc
+ln -sih ~/env/dot/gitconfig ~/.gitconfig
+ln -sih ~/env/dot/gitignore_global ~/.gitignore_global
+ln -sih ~/env/dot/UserData ~/Library/Developer/Xcode/UserData
+# h does not follow a symbolic link in resolving the second argument
 
 chflags nohidden ~/Library
 
