48f16278479cd7301cf2f158570a2b7cf3c4d070
Paul Landers <paul.landers@testplant.com>
2015-04-24 09:15:43 -0600

    added rm for UserData

--- dot/setup_dots.sh
+++ dot/setup_dots.sh
@@ -4,6 +4,7 @@ ln -sih ~/env/dot/bash_profile ~/.bash_profile
 ln -sih ~/env/dot/bashrc ~/.bashrc
 ln -sih ~/env/dot/gitconfig ~/.gitconfig
 ln -sih ~/env/dot/gitignore_global ~/.gitignore_global
+rm -rf ~/Library/Developer/Xcode/UserData
 ln -sih ~/env/dot/UserData ~/Library/Developer/Xcode/UserData
 # h does not follow a symbolic link in resolving the second argument
 
