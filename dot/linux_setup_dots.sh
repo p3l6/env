d4864720ae7e61e44eab6062929927c88daf5e57
Paul Landers <paul.landers@testplant.com>
2015-05-28 14:59:41 -0600

    added a separate setup file for linux

---
+++ dot/linux_setup_dots.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ln -si ~/env/dot/bash_profile ~/.bash_profile
+ln -si ~/env/dot/bashrc ~/.bashrc
+ln -si ~/env/dot/gitconfig ~/.gitconfig
+ln -si ~/env/dot/gitignore_global ~/.gitignore_global
+
