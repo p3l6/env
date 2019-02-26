1d3afa1f4ff47f643e144277ed57130478576e47
Paul Landers <paul.landers@eggplant.io>
2019-02-26 12:14:44 -0700

    Move functions to their own files. Correct a couple for zsh syntax

--- dot/zsh/aliases.zsh
+++ dot/zsh/aliases.zsh
@@ -1,19 +1,14 @@
 ## navigation
-function up { path=""; if [ $1 ]; then for i in $(seq 1 $1); do path=$path"../"; done; else path="../"; fi; cd $path; } # usage: $up 2
-alias back='cd -'  # previously: 'cd $OLDPWD'
+alias back='cd -'
+alias cdf='finder'
 function cdl { cd $@   &&   ls -halF; } # change to a directory and print out contents
 function mkcd { mkdir $@   &&  cd $@; }
-alias desk='cd ~/Desktop'
-alias doc='cd ~/Documents'
-alias down='cd ~/Downloads'
-function var { cd $WRKSP/$@/; }
 
 eval $(thefuck --alias)
 
 ## command shortcuts
 alias which='type -a'
 alias dush='du -sh'
-function wtf () ( shopt -s dotglob; du -shc * | sort -h; )
 alias ip='ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2'
 alias top='open -a Activity\ Monitor.app'
 alias igrep='egrep -i'
@@ -27,14 +22,3 @@ alias lc='ls -halFG'
 alias proj='open *.xc[ow][do][er][pk]*' # .xcodeproj or .xcworkspace
 alias edit='vim'
 alias readme='less *[Rr][Ee][Aa][Dd]*[Mm][Ee]*'
-
-## cd to current finder window
-cdf() {
-    target=`osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)'`
-    if [ "$target" != "" ]; then
-        cd "$target"; pwd
-    else
-        echo 'No Finder window found' >&2
-    fi
-}
-alias finder='cdf'
