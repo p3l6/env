a928bb97613cd7fd73092e154f6da0706544755a
Paul Landers <planders@statestitle.com>
2020-03-03 16:27:12 -0500

    more convert from checkout to switch

--- dot/git/aliases.zsh
+++ dot/git/aliases.zsh
@@ -1,6 +1,6 @@
 alias g='git'
 alias gs='git status -sb'
-alias co='echo Use git switch (sw)'
+alias co='echo Use git switch \(sw\)'
 alias sw='git switch'
 alias unpushed='git log @{upstream}..'
 alias unpulled='git fetch; git log ..@{upstream}'
