9fcd0cd989e02cbba193e6118e0a219845425b36
Paul Landers <planders@statestitle.com>
2020-04-20 15:39:28 -0400

    Don't add all dirs to fpath

--- dot/zsh/functions.zsh
+++ dot/zsh/functions.zsh
@@ -1,13 +1,13 @@
 # Find all functions
 fpath=($DOTFILES/dot/functions $fpath)
 autoload -U $DOTFILES/dot/functions/*(:t)
-#add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($DOTFILES/dot/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
 #add homebrew's site functions for completion
 if test -f '$(brew --prefix)/share/zsh/site-functions/'
 then
   fpath=($(brew --prefix)/share/zsh/site-functions/ $fpath)
 fi
+
 # Use direnv if it exists
 if type direnv &> /dev/null; then
   eval "$(direnv hook zsh)"
