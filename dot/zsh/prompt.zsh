4cd97e5fe196d089534839487ed7b07cefc85cf5
Paul Landers <planders@statestitle.com>
2020-09-02 19:00:04 -0400

    zsh prompt overhaul!

--- dot/zsh/prompt.zsh
+++ dot/zsh/prompt.zsh
@@ -1,37 +1,85 @@
-autoload colors && colors
+zmodload zsh/datetime
 
-prompt_git_branch () {
-  # first option branch name, second option is detached head
-  _br=$(git symbolic-ref -q --short HEAD 2> /dev/null || git  rev-parse --short HEAD 2> /dev/null)
-  if [ ! -z $_br ]; then echo "%{$fg[green]%}[$_br]%{$reset_color%}" ; fi
+ZLE_RPROMPT_INDENT=0
+
+# Formatting:
+#  Color: %F{red} %f
+#   Options: black red green yellow blue magenta cyan white default
+#   Or by number: 0-255
+#  Background: %K{red} %k
+#  Underline: $U $u
+#  Bold: $B $b
+# Other docs: http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html
+
+prompt_git_info() {
+  local gs=$(git status --porcelain=v2 --branch 2> /dev/null)
+  if [[ -z $gs ]]; then echo "%F{black}[not git]%f"; return ; fi
+
+  local branch=$(grep '^# branch.head' <<< "$gs" | cut -c 15-)
+  if [[ $branch == '(detached)' ]]; then
+    branch=$(grep '^# branch.oid' <<< "$gs" | cut -c 14-21);
+  fi
+
+  local staged=$(grep    -c '^. M.' <<< "$gs")
+  local modified=$(grep  -c '^. .M' <<< "$gs")
+  local deleted=$(grep   -c '^. .D' <<< "$gs")
+  local untracked=$(grep -c '^?' <<< "$gs")
+
+  local info="%F{green}[$branch%f"
+  (( staged )) && info+="%F{green}+$staged%f"
+  (( deleted )) && info+="%F{red}-$deleted%f"
+  (( modified )) && info+="%F{yellow}!$modified%f"
+  (( untracked )) && info+="%F{yellow}?$untracked%f"
+
+  echo "$info%F{green}]%f"
 }
 
-prompt_directory_name () {
-  # %/     Current working directory.
-  # If an integer follows the ‘%’, it specifies a number of trailing components of the current working directory to show;
-  # zero means the whole path. A negative integer specifies leading components, i.e. %-1d specifies the first component.
-  echo "%{$fg[cyan]%}%1~%{$reset_color%}"
+LAST_COMMAND_DURATION=0
+LAST_PROMPT_TIME=$EPOCHSECONDS
+prompt_exec_time() {
+  if ((LAST_COMMAND_DURATION > 0)); then
+    # TODO split out mins
+    echo "took $LAST_COMMAND_DURATION sec "
+  fi
 }
 
-prompt_time() {
-  echo "%{$fg[red]%}%@%{$reset_color%}"
+prompt_dir_name () { echo "%F{cyan}%1~%f" }
+prompt_path()      { echo "%F{cyan}%~%f" }
+prompt_time()      { echo "%F{yellow}%D{%L:%M%p}%f" }
+prompt_exit_code() { echo "%F{red}%(?..(%?%))%f" }
+prompt_bg_jobs()   { echo "%F{white}%(1j.[bg jobs: %j].)%f" }
+prompt_char()      { echo "%F{magenta}$ %f" }
+
+# This supports three propmpt positions
+#   upper
+#   lower       right
+# Support for upper-right is complicated but could look like: https://gist.github.com/romkatv/2a107ef9314f0d5f76563725b42f7cab
+
+UPPER_PROMPT='$(prompt_dir_name) $(prompt_git_info) $(prompt_path)'
+LOWER_PROMPT='$(prompt_exit_code)$(prompt_bg_jobs)$(prompt_char)'
+RIGHT_PROMPT='$(prompt_exec_time)$(prompt_time)'
+LOGGED_PROPMT='$(prompt_dir_name) $(prompt_time) $(prompt_char)'
+
+precmd() {
+  local NEWLINE=$'\n'
+  # Reset prompts modified when accepting line
+  LAST_COMMAND_DURATION=$((EPOCHSECONDS - LAST_PROMPT_TIME))
+  PROMPT="${NEWLINE}${UPPER_PROMPT}${NEWLINE}${LOWER_PROMPT}"
+  RPROMPT=$RIGHT_PROMPT
+
+  # Unset the tab title when displaying a new prompt. ie, clears ssh connection info
+  print -Pn "\e]2;%1/\a"
 }
-
-prompt_status() {
-  # conditional substring:   %(x.true-text.false-text)
-  echo "%{$fg[red]%}%(?..(%?%))%{$reset_color%}"
-}
-
-# [branch]:folder$
-export PROMPT='$(prompt_git_branch):$(prompt_directory_name)$(prompt_status)$ '
-export RPROMPT='$(prompt_time)'
-
-# Unset the tab title when displaying a new prompt. ie, clears ssh connection info
-precmd() { print -Pn "\e]2;%1/\a" }
 
 reset-prompt-and-accept-line() {
-    zle reset-prompt
-    zle accept-line
+  PROMPT=$LOGGED_PROPMT
+  RPROMPT=''
+  LAST_PROMPT_TIME=$EPOCHSECONDS
+
+  zle reset-prompt
+  zle accept-line
 }
 zle -N reset-prompt-and-accept-line
 bindkey '^m' reset-prompt-and-accept-line
+
+# TODO modify other prompts? 2,3,4,spelling ? http://zsh.sourceforge.net/Doc/Release/Parameters.html
