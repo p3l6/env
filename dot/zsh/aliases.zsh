ab35493a2010e9d553da009c2cf4412574c06cf1
Paul Landers <paul.landers@doma.com>
2022-10-28 14:56:47 -0400

    misc changes

--- dot/zsh/aliases.zsh
+++ dot/zsh/aliases.zsh
@@ -13,6 +13,7 @@ alias which='type -a'
 alias dush='du -sh'
 alias ip='ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2'
 alias diff='diff -u'
+alias now='echo $(date +%s) seconds'
 
 ## search shortcuts
 alias igrep='egrep -i'
