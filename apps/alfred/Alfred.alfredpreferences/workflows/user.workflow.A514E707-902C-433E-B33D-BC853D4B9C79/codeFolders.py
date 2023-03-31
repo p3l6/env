ab7e8e30c82a6dbeff75ae33e6f4f66ef9caf7c3
Paul Landers <paul@boombox.io>
2023-03-31 17:18:34 -0400

    Update alfred code workflow

--- apps/alfred/Alfred.alfredpreferences/workflows/user.workflow.A514E707-902C-433E-B33D-BC853D4B9C79/codeFolders.py
+++ apps/alfred/Alfred.alfredpreferences/workflows/user.workflow.A514E707-902C-433E-B33D-BC853D4B9C79/codeFolders.py
@@ -6,32 +6,55 @@ if len(sys.argv) <= 1:
   print("Provide argument: workpace directory")
   exit(2)
 
-workspace_path = os.path.expanduser(sys.argv[1])
+allowFork = allowXcode = allowVscode = False
+if len(sys.argv) > 2:
+  if sys.argv[2] == "Fork.app": allowFork = True
+  elif sys.argv[2] == "Xcode.app": allowXcode = True
+  elif sys.argv[2] == "Visual Studio Code.app": allowVscode = True
+else:
+  allowFork = allowXcode = allowVscode = True
 
-folders = [f for f in os.listdir(workspace_path) if os.path.isdir(os.path.join(workspace_path,f))]
 
-def alfred_item(name, path):
+def xcode_item(name, path):
   return {
-    "uid": path,
-    "type": "file",
-    "title": name,
-    "subtitle": path,
-    "arg": path,
-    "autocomplete": name,
-    "icon": {
-      "type": "fileicon",
-      "path": path
-    }
+    "uid": "xc"+path, "type": "file:skipcheck",
+    "title": name, "subtitle": path,
+    "arg": ["Xcode.app", path], "autocomplete": name,
+    "icon": { "path": "./xcode.tiff" }
   }
 
-def find_vscode(path):
-  return [f for f in os.listdir(path) if os.path.splitext(f)[1] == '.code-workspace']
+def fork_item(name, path):
+  return {
+    "uid": "git"+path, "type": "file:skipcheck",
+    "title": name, "subtitle": path,
+    "arg": ["Fork.app", path], "autocomplete": name,
+    "icon": { "path": "./fork.tiff" }
+  }
+
+def vscode_item(name, path):
+  return {
+    "uid": "vs"+path, "type": "file:skipcheck",
+    "title": name, "subtitle": path,
+    "arg": ["Visual Studio Code.app", path], "autocomplete": name,
+    "icon": { "path": "./vscode.tiff" }
+  }
+
+workspace_path = os.path.expanduser(sys.argv[1])
+
+top_level_folders = [f for f in os.scandir(workspace_path) if f.is_dir()]
 
 items = []
 
-for f in folders:
-  path = os.path.join(workspace_path, f)
-  items.append(alfred_item(f, path))
-  items.extend(alfred_item(w, os.path.join(path,w)) for w in find_vscode(path))
+for top_entry in top_level_folders:
+  if allowVscode: items.append(vscode_item(top_entry.name, top_entry.path))
+  for child_entry in os.scandir(top_entry.path):
+    if allowVscode and os.path.splitext(child_entry.name)[1] == '.code-workspace':
+      items.append(vscode_item(child_entry.name, child_entry.path))
+    elif allowXcode and (os.path.splitext(child_entry.name)[1] == '.xcodeproj' or os.path.splitext(child_entry.name)[1] == '.xcworkspace'):
+      items.append(xcode_item(top_entry.name, child_entry.path))
+      items.append(xcode_item(child_entry.name, child_entry.path))
+    elif allowFork and child_entry.name == '.git':
+      items.append(fork_item(top_entry.name, top_entry.path))
+
 
 print(json.dumps({"items": items}))
