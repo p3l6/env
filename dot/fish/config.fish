3366c9779ec19b9371a9c74b916c2a5c410fa2da
Paul Landers <paul@boombox.io>
2024-06-27 16:23:55 -0400

    Color schemes and nano keybindings

--- dot/fish/config.fish
+++ dot/fish/config.fish
@@ -21,4 +21,9 @@ and test -e $HOME/.iterm2_shell_integration.fish
 and source $HOME/.iterm2_shell_integration.fish
 
 string match -q "vscode" "$TERM_PROGRAM"
-and source (code --locate-shell-integration-path fish)
+and test -e $HOME/.iterm2_shell_integration.fish
+and source $HOME/.iterm2_shell_integration.fish
+
+# Vscode's built in integration doesn't work with my transient prompt
+# however, vscode also understands iterm's prompt markers, so just use that.
+# and source (code --locate-shell-integration-path fish)
