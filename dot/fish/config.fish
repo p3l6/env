988b85b3aaa3a270713c8ad57955b6479d56e2d7
Paul Landers <paul@thelanders.family>
2026-03-12 11:20:03 -0400

    Update fish and ghostty

--- dot/fish/config.fish
+++ dot/fish/config.fish
@@ -1,2 +1,5 @@
 # Note: Fish loads this file after everything in the conf.d/ directory,
 #   so no shared variables can be defined here.
+
+fish_config theme choose dracula
+set --global fish_key_bindings fish_default_key_bindings
--- dot/fish/functions/fish_prompt.fish
+++ dot/fish/functions/fish_prompt.fish
@@ -1,10 +1,7 @@
+set -g fish_transient_prompt 1
+
 function fish_prompt
     set -l last_status $status
-
-    if test "$TERM_PROGRAM" = "WarpTerminal"
-        echo '> '
-        return
-    end
 
     set -g __fish_git_prompt_show_informative_status yes
     set -g __fish_git_prompt_showdirtystate yes
@@ -22,8 +19,8 @@ function fish_prompt
         set stat (set_color red)"[$last_status] "(set_color normal)
     end
 
-    if test "$TRANSIENT" = "1"
-        printf \e\[0J # https://github.com/fish-shell/fish-shell/issues/8418
+    # Check if it's a transient or final prompt
+    if contains -- --final-rendering $argv
         string join '' -- (set_color cyan) (basename $PWD) ' ' (set_color yellow) (date "+%-l:%M%p") (set_color magenta) ' > ' (set_color normal)
     else
         echo
@@ -31,20 +28,3 @@ function fish_prompt
         string join '' -- $stat (set_color magenta) '〉' (set_color normal)
     end
 end
-
-
-function reset-transient --on-event fish_postexec
-    set -g TRANSIENT 0
-end
-
-function maybe_execute
-    if commandline --is-valid
-        set -g TRANSIENT 1
-        commandline -f repaint
-    else
-        set -g TRANSIENT 0
-    end
-    commandline -f execute
-end
-
-bind \r maybe_execute
