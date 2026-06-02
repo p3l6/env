1d80709c8e11c345b3d22fdb75655a0d209c64a3
Paul Landers <paul@thelanders.family>
2026-06-02 14:02:53 -0400

    Mist color themes

--- dot/fish/conf.d/lsd.fish
+++ dot/fish/conf.d/lsd.fish
@@ -1,2 +1,3 @@
 abbr --add lsdl lsd -l
 abbr --add lsdlt lsd -l --tree
+abbr --add lt l --tree
--- dot/fish/config.fish
+++ dot/fish/config.fish
@@ -1,5 +1,29 @@
 # Note: Fish loads this file after everything in the conf.d/ directory,
 #   so no shared variables can be defined here.
 
-fish_config theme choose dracula
 set --global fish_key_bindings fish_default_key_bindings
+
+# Theme
+
+set --global fish_color_normal normal
+set --global fish_color_history_current --bold
+set --global fish_color_search_match brwhite --bold --background=brblue
+set --global fish_color_valid_path --underline=single
+
+set --global fish_color_autosuggestion brblack
+set --global fish_color_comment brblack
+
+set --global fish_color_quote red
+set --global fish_color_error brred
+
+set --global fish_color_command cyan
+
+set --global fish_color_escape yellow
+set --global fish_color_option yellow
+
+set --global fish_color_param blue
+
+set --global fish_color_keyword magenta
+set --global fish_color_end magenta
+set --global fish_color_operator magenta
+set --global fish_color_redirection magenta
